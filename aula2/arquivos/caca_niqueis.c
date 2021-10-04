251a5fca83fee2eaf0a774e2c8cc2199c9340537
DinossauroBebado <guipsilva32@gmail.com>
2021-10-04 20:09:37 -0300

    this will work

---
+++ aula2/arquivos/caca_niqueis.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "queue.c"
+int main()
+{
+
+   /*TERMINAR*/
+   /*Criar três filas para simular uma máquina caça-níqueis!*/
+   /*Obrigatório usar as funções vistas em aula!*/
+   /*Pode criar funções na TAD fila caso seja necessário, por exemplo, mostrar primeiro elemento da fila!*/
+   /*Pode criar funções adicionais para girar uma fila!*/
+   /*...*/
+   Fila *f = createcreate_queue(2);
+   return 0;
+}
