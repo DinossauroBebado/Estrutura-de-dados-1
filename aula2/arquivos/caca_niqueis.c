4c9d36463702f289bd0824f52f546426a85d1997
DinossauroBebado <guipsilva32@gmail.com>
2021-10-06 16:44:13 -0300

    ex aula2

--- aula2/arquivos/caca_niqueis.c
+++ aula2/arquivos/caca_niqueis.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+// Initialization, should only be called once.
+
 #include "queue.c"
+
+#define size 11
+#define spin 8
 int main()
 {
 
@@ -9,6 +17,44 @@ int main()
    /*Pode criar funções na TAD fila caso seja necessário, por exemplo, mostrar primeiro elemento da fila!*/
    /*Pode criar funções adicionais para girar uma fila!*/
    /*...*/
-   Fila *f = createcreate_queue(2);
+   srand(time(NULL));
+
+   Fila *f1 = create_queue(size);
+   Fila *f2 = create_queue(size);
+   Fila *f3 = create_queue(size);
+
+   int r1 = (rand() % (9 - 1)) + 1;
+   int r2 = (rand() % (9 - 1)) + 1;
+   int r3 = (rand() % (9 - 1)) + 1;
+
+   for (int i = 1; i < 10; i++)
+   {
+      enqueue(f1, i);
+      enqueue(f2, i);
+      enqueue(f3, i);
+   }
+   for (int p = 0; p < spin; p++)
+   {
+      spinn(f1, r1);
+      spinn(f2, r2);
+      spinn(f3, r3);
+
+      printf("%d", show_first(f1));
+      printf("%d", show_first(f2));
+      printf("%d\n", show_first(f3));
+   }
+
+   if (show_first(f1) == show_first(f2) && show_first(f2) == show_first(f3))
+   {
+      printf("-------------------| You WIN |-----------------------\n");
+   }
+   else
+   {
+      printf("-------------------| Play Again ? |-----------------------\n");
+   }
+
+   free_queue(f1);
+   free_queue(f2);
+   free_queue(f3);
    return 0;
 }
--- aula2/arquivos/queue.c
+++ aula2/arquivos/queue.c
@@ -1,50 +1,69 @@
 #include "queue.h"
 
-Fila* create_queue (int tam) {
+Fila *create_queue(int tam)
+{
    Fila *f = (Fila *)malloc(sizeof(Fila));
    f->ini = 0;
    f->fim = 0;
    f->tam = tam;
-   f->vetor = (int*)malloc(tam*sizeof(int));
+   f->vetor = (int *)malloc(tam * sizeof(int));
    return f;
 }
 
 /*Função para enfileirar um elemento de uma fila 
   circular implementada através de um vetor!*/
-void enqueue (Fila *f, int e) {
-   if (full(f)) {
+void enqueue(Fila *f, int e)
+{
+   if (full(f))
+   {
       printf("fila cheia: capacidade esgotada!");
       exit(1); /*aborta programa*/
    }
-   /*TERMINAR!*/ /*insere elemento*/
-   /*TERMINAR!*/ /*incremento*/
+
+   f->vetor[f->fim] = e;
+   f->fim = (f->fim + 1) % f->tam;
 }
 
 /*Função para desenfileirar um elemento de uma fila 
   circular implementada através de um vetor!*/
-int dequeue (Fila *f) {
-   if (empty(f)) {
+int dequeue(Fila *f)
+{
+   if (empty(f))
+   {
       printf("fila vazia!");
       exit(1); /*aborta programa*/
    }
-   /*TERMINAR!*/ /*guarda o elemento*/
-   /*TERMINAR!*/ /*incremento*/
-   /*TERMINAR!*/ /*retorna elemento*/
-}
+   int e = f->vetor[f->ini];
 
-int empty (Fila *f) {
+   f->ini = (f->ini + 1) % f->tam;
+
+   return e;
+}
+int empty(Fila *f)
+{
    return (f->ini == f->fim);
 }
 
-int full (Fila *f) {
+int full(Fila *f)
+{
    return (f->ini == ((f->fim + 1) % f->tam));
 }
 
-void free_queue (Fila *f) {
+void free_queue(Fila *f)
+{
    free(f->vetor);
    free(f);
 }
 
+int show_first(Fila *f)
+{
+   return f->vetor[f->ini];
+}
 
-
-
+void spinn(Fila *f, int j)
+{
+   for (int s = 0; s < j; s++)
+   {
+      enqueue(f, dequeue(f));
+   }
+}
